5b18bfd633c3f2d432ee474ae6332668d483c9e7
ram <rambabu0006@gmail.com>
2017-11-15 19:11:40 +0800

    group entity changes

--- src/main/java/com/rainier/gc/system/gc/model/Group.java
+++ src/main/java/com/rainier/gc/system/gc/model/Group.java
@@ -1,14 +1,10 @@
 package com.rainier.gc.system.gc.model;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 
