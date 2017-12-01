a0dc5ad9a8689bf23d36ebe5d323b8a3aa55bf68
Rahul Kumar Pamidi <pamidirahul.20@gmail.com>
2017-12-01 22:08:26 +0530

    Updating with Tomcat Deployment Issue

--- src/main/java/com/rainier/gc/system/gc/GcApplication.java
+++ src/main/java/com/rainier/gc/system/gc/GcApplication.java
@@ -2,9 +2,10 @@ package com.rainier.gc.system.gc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GcApplication {
+public class GcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcApplication.class, args);
